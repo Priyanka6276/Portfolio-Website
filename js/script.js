a12aa43e06fa5b44b3ec29b174056ed3ebcef7c3
Priyanka6276 <priyankachanbrasegaran@gmail.com>
2023-01-12 15:23:02 -0500

    modal static working

---
+++ js/script.js
@@ -0,0 +1,20 @@
+const modal = document.querySelector(".modal")
+const previews = document.querySelectorAll(".image-container")
+const original = document.querySelector(".modal-img")
+const imgText = document.querySelector(".caption")
+
+previews.forEach(preview => {
+    preview.addEventListener("click", () => {
+        modal.classList.add("open")
+        original.classList.add("open")
+    })
+})
+
+modal.addEventListener("click", (e) => {
+    if(e.target.classList.contains("modal")){
+        modal.classList.remove("open")
+        original.classList.remove("open")
+    }
+})
+
+
